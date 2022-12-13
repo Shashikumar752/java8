af7dbf64d3eb3ed3b73775491813652b7b000a47
Shashi Kumar <shashisingh752@gmail.com>
2022-12-13 17:49:29 +0530

    Updated FunctionFI.java

--- src/main/java/com/shashi/lambda/sam/FunctionFI.java
+++ src/main/java/com/shashi/lambda/sam/FunctionFI.java
@@ -2,7 +2,6 @@ package com.shashi.lambda.sam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
