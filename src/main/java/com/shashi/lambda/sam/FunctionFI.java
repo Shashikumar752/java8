aca50d4b4a37d858390581d8367f576b5c0f74f2
Shashi Kumar <shashisingh752@gmail.com>
2022-12-13 17:47:41 +0530

    added implementation of unary and binary operator function

--- src/main/java/com/shashi/lambda/sam/FunctionFI.java
+++ src/main/java/com/shashi/lambda/sam/FunctionFI.java
@@ -2,11 +2,16 @@ package com.shashi.lambda.sam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import com.shashi.lambda.beans.Student;
 import com.shashi.lambda.dao.StudentDB;
@@ -33,6 +38,35 @@ public class FunctionFI {
         return sListCopy;
     };
 
+    public static BiFunction<List<Student>, Predicate<Student>, List<Student>> bifFilter =
+            (sList, pS) -> {
+                List<Student> sListCopy = new ArrayList<Student>(sList);
+                sList.forEach((s) -> {
+                    if (!(pS.test(s))) {
+                        sListCopy.remove(s);
+                    }
+                });
+                return sListCopy;
+            };
+    // Implemetation is same as fFilter as input and output is of same type
+    public static UnaryOperator<List<Student>> uoF = (sList) -> {
+        List<Student> sListCopy = new ArrayList<Student>(sList);
+        sList.forEach((s) -> {
+            if (!(PredicateFI.sP.or(PredicateFI.sPr).test(s))) {
+                sListCopy.remove(s);
+            }
+        });
+        return sListCopy;
+    };
+
+    // Implemetation is same as fFilter as input and output is of same type
+    public static BinaryOperator<List<Student>> boF = (sList, asList) -> {
+        // List<Student> sListCopy = new ArrayList<Student>(sList);
+        sList.addAll(asList);
+
+        return sList;
+    };
+
     public static void main(String[] args) {
         List<Student> sTList = StudentDB.createStudents();
         System.out.println(sTList.toString());
@@ -45,6 +79,20 @@ public class FunctionFI {
 
         System.out.println("after applying and then function");
         System.out.println(fFilter.andThen(f).apply(sTList));
+
+        System.out.println("after applying unaryoperator funtion, result should be same as above");
+        System.out.println(uoF.andThen(f).apply(sTList));
+
+        System.out.println("Bifunction implementation result");
+        System.out.println(bifFilter.andThen(f).apply(sTList, PredicateFI.sP));
+
+        List<Student> sList1 = SupplierFI.supp.get();
+        System.out.println("sList1: " + sList1.toString());
+        List<Student> sList2 = SupplierFI.supp1.get();
+        System.out.println("sList2: " + sList2.toString());
+        System.out.println("Binary Operator implementation result");
+        System.out.println(boF.apply(sList1, sList2));
+
 
         Arrays.stream("BANANA".split(""))
                 .collect(Collectors.groupingBy(s -> s, LinkedHashMap::new, Collectors.counting()))
